c1701d78a7487be4e40f8553c9ca8f5abe74201a
yosh-25 <yoshi.okada25@gmail.com>
2024-02-03 23:49:42 +0900

    done

---
+++ pages/about/index.js
@@ -0,0 +1,10 @@
+import Content from "../components/content";
+import Header from "../components/header";
+
+export default function About() {
+  return (
+    <Content>
+      <Header title="About"/>
+    </Content>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,36 @@
 import Head from "next/head";
-import Styles from "../styles/Home.module.css"
+import Link from "next/link";
+import Content from "./components/content";
+import useSWR from "swr";
+
+import Styles from "../styles/Home.module.css";
+import Header from "./components/header";
+
 
 export default function Home() {
   let title = "tomosta";
+  const fetcher = (...args) => fetch(...args).then(res => res.json())
+  const {data, error} = useSWR('/api/message', fetcher);
+  if(error) return <div>failed to load</div>
+  if(!data) return <div>loading...</div>
+
   return (
-    <>
+    <Content>
       <Head>
         <title>{title}</title>
       </Head>
-      <h1 className={Styles.mytitle} style={{ backgroundColor: `#9f9` }}>
-        {title}
-      </h1>
-      <p>学ぶをちゃんと</p>
+      <Header title={title} />
+      <p>{data.message}</p>
+      <div>
+        <Link href="/about">About</Link>
+      </div>
       <style jsx>
         {`
           h1 {
             color: white;
           }
-
         `}
       </style>
-    </>
+    </Content>
   );
 }
